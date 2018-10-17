66727c38b5c6392f688d849de265b9e03cd3e9f8
Brody Lemke <500161066@fvtc.edu>
2018-10-16 20:17:22 -0500

    Added add col, and started context menu.

--- Spreadsheet/spreadsheet.js
+++ Spreadsheet/spreadsheet.js
@@ -8,6 +8,12 @@ var rowLetter;
 
 var settingUp = true;
 
+
+//Context Menu variables
+var menu;
+var menuState = 0;
+var active = "contextMenuActive";
+
 //wait until document is loaded to do jquery
 $(document).ready(function(){
   //Set-up the document
@@ -32,13 +38,18 @@ $(document).ready(function(){
 
   //turn of setting up for animations when rows are added
   settingUp = false;
+
+  contextMenuPrep();
+
+  //after set up, set the context menu into a variable
+  menu = document.querySelector(".contextMenu");
 });
 
 
 
 
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Converting letters and numbers
 
 function numToString(num){
   //From https://stackoverflow.com/questions/11089399/count-with-a-b-c-d-instead-of-0-1-2-3-with-javascript
@@ -49,9 +60,20 @@ function numToString(num){
   return pow ? numToString(pow) + out : out;
 }
 
+function stringToNum(str) {
+    "use strict";
+    var out = 0, len = str.length, pos = len;
+    while (--pos > -1) {
+        out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - 1 - pos);
+    }
+    return out;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Adding Columns and rows
 
 function addRowToBottom(){
   var curRowNum =  $('#spreadsheetContainer .row').length;
+  var curColNum =  $('#spreadsheetContainer .row:first .col').length;
   console.log(curRowNum);
 
     //start a row
@@ -59,14 +81,103 @@ function addRowToBottom(){
   //the Left column with the numberKey inside the new row
   $(".row#"+(curRowNum)).append('<div class="numberKey">' + (curRowNum) + '</div>');
 
-  for (var j = 0; j < numCols; j++){
+  for (var j = 0; j < curColNum; j++){
     //add columns/cells to the row
     var rowLetter = numToString(j+1);
     if(settingUp){
       $(".row#"+(curRowNum)).append('<input class="col" id="' + rowLetter + '">');
     } else{
       //add animation if after the setup
-      $(".row#"+(curRowNum)).hide().append('<input class="col" id="' + rowLetter + '">').slideDown(250);
+      $(".row#"+(curRowNum)).hide().append('<input class="col" id="' + rowLetter + '">').slideDown(100);
     }
   }
+  //scroll to bottom
+  $("body").animate({scrollTop: $("#spreadsheetContainer").prop("scrollHeight")}, 100);
 }
+
+
+function addColToEnd(){
+  //find the number of columns
+  var curColNum =  $('#spreadsheetContainer .row:first .col').length + 1;
+
+  //get next letter
+  var rowLetter = numToString(curColNum);
+
+  //add the key letter
+  $("#key").append('<div class="col" id="' + rowLetter + '" > ' + rowLetter + ' </div>');
+
+  //add column to every row except for the first
+  $(".row:not(:first)").append('<input class="col" id="' + rowLetter + '">').slideDown(1000);;
+
+  var leftPos = $('body').scrollLeft()
+  $("body").animate({scrollLeft: leftPos + 400}, 800);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Context menu
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function contextMenuPrep() {
+  //based on this https://www.sitepoint.com/building-custom-right-click-context-menu-javascript/
+
+  "use strict";
+
+  //create an array of all the columns in the key row and rows with a numberKey
+  var columns = $("#key .col");
+  var rows = $(".row .numberKey");
+
+  //loop through the columns and add event listeners
+  for ( var i = 0, len = columns.length; i < len; i++ ) {
+    var column = columns[i];
+    contextMenuListener(column);
+  }
+
+  //loop through the rows and add even listeners
+  for ( var i = 0, len = rows.length; i < len; i++ ) {
+    var row = rows[i];
+    contextMenuListener(row);
+  }
+
+  //add even listener to the document to close context menu
+  document.addEventListener( "click", function(e) {
+    //click anywhere to close the context menu
+    toggleMenuOff();
+  });
+
+}
+
+  //function to add event listeners
+function contextMenuListener(el) {
+    el.addEventListener( "contextmenu", function(e) {
+    e.preventDefault();
+    toggleMenuOn();
+    });
+}
+
+//Turn the menu on, set it's display to block using a new class
+function toggleMenuOn() {
+    if ( menuState !== 1 ) {
+      menuState = 1;
+      menu.classList.add(active);
+    }
+  }
+
+function toggleMenuOff() {
+    if ( menuState == 1 ) {
+      menuState = 0;
+      menu.classList.remove(active);
+    }
+  }
+
+  function contextListener(){
+
+  }
+
+  function clickListener(){
+
+  }
+
+  function keyUpListener(){
+
+
+  }
